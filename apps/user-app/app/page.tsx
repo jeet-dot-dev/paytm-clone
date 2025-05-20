e8027be8f4a218ce075e1a3cc9fddbb6f482906a
jeet-dot-dev <jeet999dev@gmail.com>
2025-05-20 15:18:17 +0530

    bootstarp my project

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-500">Hello Jeet!</h1>
+    </div>
+  )
+}
+
+export default page
